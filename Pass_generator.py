497bb2aa5e9cdefd2b69ab3d7e13b9b7d468b0db
Shriram Bhardwaj <121400894+27Shri03@users.noreply.github.com>
2023-01-03 17:07:10 +0530

    Add files via upload

---
+++ Pass_generator.py
@@ -0,0 +1,18 @@
+import random  # To generate or to choose random numbers.
+import string  # To generate a password.
+
+# User will enter the length of the password.
+
+n = int(input("Enter the length of the password: "))
+
+# Source string is used to combine the letters and digits.
+
+source = string.ascii_letters + string.digits
+
+# We will choose each character randomly from the source string and add it into our empty string using a for loop till n.
+
+password = ''.join((random.choice(source) for i in range(n)))
+
+# Now we will print our password.
+
+print("\nRandom password with length", n, "is: ", password)
